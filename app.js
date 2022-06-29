98777dddea746ef8b05a620298603882b2971b98
Austin Brock <austinjbrock@gmail.com>
2022-06-28 21:40:40 -0700

    changed function name and url meta-data

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-async function catchRainbow(){
+async function getRandomPhoto(){
    //grab URL we are pulling from
-   const response = await fetch('https://picsum.photos/400?rainbow');
+   const response = await fetch('https://picsum.photos/400');
    //run blob function when we get the response
    const blob = await response.blob();
    // Render data onto html page with current img
-   document.getElementById('rainbow').src = URL.createObjectURL(blob)
+   document.getElementById('photo-src').src = URL.createObjectURL(blob)
 }
 //call the function to get started
-catchRainbow()
+getRandomPhoto()
