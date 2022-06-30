35433b10ab64fb8a19456ded65a089252d6d501f
Austin Brock <austinjbrock@gmail.com>
2022-06-29 22:12:37 -0700

    hooked up event listener

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+const displayBtn = document.getElementById('photo-btn')
+
+
 async function getRandomPhoto(){
    //grab URL we are pulling from
-   const response = await fetch('https://picsum.photos/400');
+   const response = await fetch('https://picsum.photos/600');
    //run blob function when we get the response
    const blob = await response.blob();
    // Render data onto html page with current img
    document.getElementById('photo-src').src = URL.createObjectURL(blob)
+
 }
 //call the function to get started
-getRandomPhoto()
+
+displayBtn.addEventListener('click',getRandomPhoto)
